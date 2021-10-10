5c60a3a85218ab6ffcf45909ba9874e7c31f50e5
Akash Biswas <64808720+akshbswas98@users.noreply.github.com>
2021-10-10 13:26:51 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 function App() {
+  const onSubmit = data => console.log(data);
+  const [loginState, setLoginState] = useState({
+    email:"",
+    password:"",
+    checkMeOut:false,
+  });
+  const { register, handleSubmit, formState: { errors } } = useForm();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+
+    <div className="container my-5">
+           <h1 className="display-1 text-decoration-underline">Sign-up page DEMO(Botspot AI)</h1>
+      <form>
+        <div className="mb-3">
+          <label for="exampleInputEmail1" className="form-label lead ">
+          Email address
+          </label>
+          <input 
+          onSubmit={handleSubmit(onSubmit)}
+          
+          type="email" 
+          className="form-control " 
+          aria-describedby="emailHelp" 
+          onChange={ (event) => {
+            const value = event.target.value;
+            setLoginState({ ...loginState, email: value });
+          } }
+    
+          />         
+          <div id="emailHelp" className ="form-text">
+          We'll never share your email with anyone else.</div>
+        </div>
+        <div className ="mb-3">
+          <label for="exampleInputPassword1" class="form-label lead">
+          Password</label>
+          <input 
+          type="password" 
+          class="form-control" 
+          id="exampleInputPassword1" 
+          onChange={ (event) => {
+            const value = event.target.value;
+            setLoginState({ ...loginState, password: value });
+          } }
+          />
+        </div>
+        <div class="mb-3 form-check">
+          <input
+           type="checkbox" 
+           class="form-check-input" 
+           id="exampleCheck1" 
+           onChange={ (event) => {
+            const value = event.target.checked;
+            setLoginState({ ...loginState, checkMeOut: value });
+          } }
+           />
+          <label class ="form-check-label" for="exampleCheck1">
+          Remember me
+          </label>
+        </div>
+        <button type="submit" class="btn btn-primary" disabled={
+          !loginState.email || !loginState.password}>Submit</button>
+      </form>
+      <footer>
+    
+      </footer>
     </div>
+    
   );
 }
 
 export default App;
+//{JSON.stringify(loginState)}
